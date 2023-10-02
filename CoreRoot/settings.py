eaa3cbe5e221e7664009ce503b392181f876211b
VinCoD <5895.2020@students.ku.ac.ke>
2023-10-02 09:39:04 +0300

    Added Custom User Model

--- CoreRoot/settings.py
+++ CoreRoot/settings.py
@@ -36,6 +36,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # My Apps
+    'core',
+    'core.user',
 ]
 
 MIDDLEWARE = [
@@ -115,3 +118,5 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+AUTH_USER_MODEL = 'core_user.User'
