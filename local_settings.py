e39622d41c240626797b2aa7afa1fde196bc5536
Odipo Otieno <5895.2020@students.ku.ac.ke>
2024-08-27 17:44:12 +0300

    Replaced db

--- local_settings.py
+++ local_settings.py
@@ -7,10 +7,10 @@ SECRET_KEY = 'django-insecure-byzwr3*z(xaiyki9+3&z#m9f&)n_fhgrt66e&bz9w^d=6-v0os
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'approved_project',
-        'USER': 'approved_project_user',
-        'PASSWORD': 'Qvgpl0x11JIUZwhFq0BjVeR4z0rAYbJQ',
-        'HOST': 'dpg-cr53so52ng1s73eajicg-a.oregon-postgres.render.com',
+        'NAME': 'postagram_api',
+        'USER': 'postagram_api_user',
+        'PASSWORD': 'taUbXnkNVaJPbnE4eHzLQOUIUFgA1Api',
+        'HOST': 'dpg-cr6u8oi3esus7393ftr0-a.oregon-postgres.render.com',
         'PORT': '5432',
     }
 }
